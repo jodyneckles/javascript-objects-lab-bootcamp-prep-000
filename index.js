272545ff4e20f81d1add0838e18c416236b4b313
Jody Neckles <jody@technative.tv>
2018-10-18 17:16:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey (object, key, value) {
+  var obj = { [key]: value }
+  var newObj = Object.assign({}, obj)
+  return delete newObj.key
+}
+
+function destructivelyDeleteFromObjectByKey(object, key, value) {
+  var object = { key: value }
+  return delete [key],[value]
+}
